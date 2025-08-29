1a01e3d7f8da523ff080c65d16bc0106a492f388
Marco Vit <marco.vit@pagopa.it>
2025-08-29 15:38:35 +0200

    style Use dotenv-flow instead of custom bash script

--- lambdas/nodejs/analytics-refresh-mv/local_run.sh
+++
@@ -1,9 +0,0 @@
-export REDSHIFT_CLUSTER_IDENTIFIER="interop-analytics-dev" 
-export REDSHIFT_DATABASE_NAME="interop_dev" 
-export REDSHIFT_DB_USER="dev_mv_refresher_user"  
-
-export VIEWS_SCHEMAS_NAMES="[\"views\", \"sub_views\"]"
-export PROCEDURES_SCHEMA="sub_views"
-
-npm run run-local
-
